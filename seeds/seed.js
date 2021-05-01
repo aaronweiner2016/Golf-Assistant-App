5b27b7f3d55ea0e916f7553a4b9eb79c39acd1e5
JWalters <joshwalters34@gmail.com>
2021-05-01 15:08:02 -0400

    updated seed.js file with appropriate files

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,37 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, GolfCourse, GolfHole, Handicap, RoundOfGolf } = require('../models');
 
-const userData = require('./userData.json');
+const userData = require('./User.json');
+const roundOfGolfData = require('./RoundOfGolf.json');
+const handicapData = require('./Handicap.json');
+const golfHoleData = require('./GolfHole.json');
+const golfCourseData = require('./GolfCourse.json');
 
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await GolfCourse.bulkCreate(golfCourseData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await GolfHole.bulkCreate(golfHoleData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Handicap.bulkCreate(handicapData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await RoundOfGolf.bulkCreate(roundOfGolfData, {
     individualHooks: true,
     returning: true,
   });
