89dbe4bd215d9171687f051fa86bba13ff4403d1
aaron weiner <aaronjacobweiner@yahoo.com>
2021-05-01 14:06:13 -0400

    layout of the ui

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// Prevent non logged in users from viewing the homepage
 router.get('/', withAuth, async (req, res) => {
   try {
     const userData = await User.findAll({
@@ -14,7 +13,7 @@ router.get('/', withAuth, async (req, res) => {
 
     res.render('homepage', {
       users,
-      // Pass the logged in flag to the template
+      name: req.session.name,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -22,14 +21,64 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
+
 router.get('/login', async (req, res) => {
-  // If a session exists, redirect the request to the homepage
   if (req.session.logged_in) {
     res.redirect('/');
     return;
   }
 
   res.render('login');
+});
+
+router.get('/sign-up', async (req, res) => {
+  res.render('sign-up', {
+    logged_in: req.session.logged_in,
+  });
+});
+
+router.get('/post-score', withAuth, async (req, res) => {
+  try {
+    res.render('post-score', {
+      name: req.session.name,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/stats', withAuth, async (req, res) => {
+  try {
+    res.render('stats', {
+      name: req.session.name,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/handicap-calc', withAuth, async (req, res) => {
+  try {
+    res.render('handicap-calc', {
+      name: req.session.name,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/new-game', withAuth, async (req, res) => {
+  try {
+    res.render('new-game', {
+      name: req.session.name,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
